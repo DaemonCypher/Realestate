9125589b84aa399abe9e4de736c6012797ddc370
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-08-18 13:36:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-from getDetails import *
 from getAdresses import *
 import os 
-import json
+import map
+import cleaner
+import downloader
+from statenames import *
+# when downloading addresses from openAddresses look for the statewide addresses file
 
+def main():
+    state = ""
+    city = ""
+    print("What region of the U.S. realestate would you like to at")
+    region = input("NE(North East), S(South), MW(Mid West),W(West)\n:")
+    region = region.lower()
+    region= region.replace(" ","")
+    print(abbreviations(region))
+    city_toggle = input("Do you want addresses from a city ")
+    print()
+    
+    
+    
+main()
 
-data = getDetails("155 BATTERY PARK DR")
-
-
-num_beds = data['payload']['numBeds']
-num_baths = data['payload']['numBaths']
-sq_ft = data['payload']['sqFt']['value']
-listing_price = data['payload']['listingPrice']
-is_serviced = data['payload']['isServiced']
-is_active = data['payload']['isActivish']
-latitude = data['payload']['latLong']['latitude']
-longitude = data['payload']['latLong']['longitude']
-price_amount = data['payload']['priceInfo']['amount']
-assembled_address = data['payload']['streetAddress']['assembledAddress']
-predicted_value = data['payload']['predictedValue']
-
-# Print the extracted information
-print(f"Number of Beds: {num_beds}")
-print(f"Number of Baths: {num_baths}")
-print(f"Square Feet: {sq_ft}")
-print(f"Listing Price: {listing_price}")
-print(f"Is Serviced: {is_serviced}")
-print(f"Is Active: {is_active}")
-print(f"Latitude: {latitude}")
-print(f"Longitude: {longitude}")
-print(f"Price Amount: {price_amount}")
-print(f"Assembled Address: {assembled_address}")
-print(f"Predicted Value: {predicted_value}")
+    
+    
+    
+    
+    
