037d6bf6fb30dfba8fde176999afd3cca202803c
Basic <god.of.gaming.901@gmail.com>
2023-09-04 18:05:29 -0500

    clean slate

--- downloader.py
+++
@@ -1,7 +0,0 @@
-""" 
-    This is a temporary place holder for downloading files from https://batch.openaddresses.io/data.
-    As of the moment there is issue with using selenium python library to navigate to the URL and download
-    the following files us-northeast, us-south, us-midwest, us-west. There may be another method to download the files
-    but as of this moment it is not possible
-    
-"""
