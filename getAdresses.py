5494d01aae56b2ae1bcd42869256f453754aead4
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-09-12 14:08:22 -0500

    Documentation to getAddress()

--- getAdresses.py
+++ getAdresses.py
@@ -2,43 +2,63 @@ import geopandas as gpd
 from alive_progress import alive_bar
 
 def getAddress(file):
-    # Reads details from the .geojson file and appends them to a dictionary
-    # for the future when developing an price history of a city,county,state
-    # will use the dictionary file for quick access of seeing the price change
-    # and updating the average price of an area
-    
-    
+    """
+    Parse and format addresses from a .geojson file.
+
+    The function reads address details from the given .geojson file and structures 
+    them in a readable format. This can be used to develop price history for various 
+    geographical entities like cities, counties, or states. The resultant address 
+    dictionary can be used for quick access and updates regarding average price 
+    changes in these areas.
+
+    Parameters:
+    - file (str): Path to the .geojson file containing address data.
+
+    Returns:
+    - dict: Dictionary with keys being the index and values being the formatted address.
+    """
+
+    # Use a progress bar to indicate reading progress.
     with alive_bar(title='Reading addresses from the file...') as loading:
         data = gpd.read_file(file)
-        address = {}
-        loading()
-    
-    with alive_bar(len(data), title='Processing Addresses',) as bar:
+        address = {}  # Dictionary to store formatted addresses.
+        loading()  # Update the loading bar.
+
+    # Use a progress bar to indicate processing progress for each address entry.
+    with alive_bar(len(data), title='Processing Addresses') as bar:
         for index, row in data.iterrows():
+            # Extract address components.
             number = row['number']
             street = row['street']
             unit = row['unit']
             city = row['city']
             district = row['district'] # county
-            region = row['region'] # state
+            region = row['region']     # state
             postcode = row['postcode']
+
+            # Initialize empty string for address formatting.
             output = ""
+
+            # Concatenate available address components.
             if number:
-                output = output + number + " "
+                output += number + " "
             if street:
-                output = output + street + " "
+                output += street + " "
             if unit:
-                output =output + unit + ", " 
+                output += unit + ", " 
             if city:
-                output = output + city + ", " 
+                output += city + ", " 
             if district:
-                output = output + district + ", "
+                output += district + ", "
             if region:
-                output = output + region + " "
+                output += region + " "
             if postcode:
-                output = output + postcode
+                output += postcode
+
+            # Add the formatted address to the dictionary.
             address[index] = output
-            
-            # Each time the loop iterates, update the progress bar.
+
+            # Update the progress bar.
             bar()
+
     return address
