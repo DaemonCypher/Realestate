b3db7646553e1a21fe8867d1ab92628c4625f5c9
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-09-12 14:11:56 -0500

    Added Documentations

--- getDetails.py
+++ getDetails.py
@@ -1,76 +1,130 @@
-
 import datetime
 import traceback
 
-
 #TODO: there is a bug with client.search where there is no url to the address provided
-async def getDetails(address,client):
+
+# seems to data can only be grabbed on address with the status
+# 'Sold (Public Records)
+# 'Sold (MLS)'
+# 'Pending'
+# 'Listing Removed'
+# 'Contingent'
+# 'Price Changed'
+# 'Listed'
+# 'Relisted'
+async def getDetailsClosedProperty(address,client):
+    """
+    Fetch details of a closed property based on its address.
+
+    Parameters:
+    - address (str): The address to search for.
+    - client: An initialized client object to interact with the property service.
+
+    Returns:
+    - dict: A dictionary containing the property's details. If an error occurs, an empty string is returned.
+    """
     try:
-        # Search for property and get the URL
+        # Search for property using its address and extract its URL.
         response = await client.search(address)
-        url = response.get('payload', {}).get('exactMatch', {}).get('url')
+        url = safe_get(response, 'payload', 'exactMatch', 'url')
         if not url:
             raise ValueError(f"No URL found for address: {address}")
-
-        # Get initial info and propertyId
+        
+        # Extract initial info and propertyId using the URL.
         initial_info = await client.initial_info(url)
-        property_id = initial_info.get('payload', {}).get('propertyId')
+        property_id = safe_get(initial_info, 'payload', 'propertyId')
         if not property_id:
             raise ValueError(f"No property ID found for URL: {url}")
 
-        # Fetch MLS data and history
-        mls_data_payload = (await client.below_the_fold(property_id)).get('payload', {})
-        history_payload = (await client.price_history(property_id)).get('payload', {})
+        # Extract MLS data and property history.
+        mls_data_payload = safe_get(await client.below_the_fold(property_id), 'payload')
+        history_payload = safe_get(await client.price_history(property_id), 'payload')
 
-        # Extract necessary data
-        event = mls_data_payload.get('propertyHistoryInfo', {}).get('events', [{}])[0]
-        timestamp_seconds = event.get('eventDate', 0) / 1000
+        # Extract the desired data from the payloads.
+        event = safe_get(mls_data_payload, 'propertyHistoryInfo', 'events', 0)
+        timestamp_seconds = safe_get(event, 'eventDate', default=0) / 1000
         date_obj = datetime.datetime.fromtimestamp(timestamp_seconds)
-
+        
         return {
-            'displayedPrice': event.get('price'),
+            'displayedPrice': event.get('price',-1),
             'history': history_payload.get('propertyTimeSeries', []),
-            'status': event.get('eventDescription'),
+            'status': event.get('eventDescription','N/A'),
             'statusDate': date_obj
         }
-        
- 
-        
-    
-        # can get info from mls_data
-        #listing_id = initial_info['payload']['listingId']
-        #property_request=client.above_the_fold(property_id,listing_id)
-        # can get data from above_the_fold
-        
-        #avm_details = client.avm_details(property_id, listing_id)
-        #price_history= client.avm_historical(property_id, listing_id)
-        #each index in history is 1 month and combined to be last 5 years
-        # so current month and going back as far as possible in the last 5 years
-        #history = price_history['payload']['propertyTimeSeries']
-        #last_update = price_history['payload']['avmUpdateDate']
 
-        #data =({'mls_data':mls_data},{'property_request':property_request},{'avm_details':avm_details},{'price_history':price_history})
-    except ValueError as e:  # Catches the custom error messages
+    except ValueError as e:  # For custom error messages
         #print(f"Error encountered: {e}")
         return ""
-    except Exception as e:  # Catches unexpected issues
+    except Exception as e:   # For unexpected issues
         #print(f"Unexpected error encountered: {e}")
         #print(traceback.format_exc())
         return ""
-
-    return ""  # This will be returned if the function hasn't returned until this point
-
-def safe_get(data, *keys):
+  
+async def getDetailsOpenedProperty(address,client):
     """
-    Safely retrieve a value from a nested dictionary.
+    Fetch details of an opened property based on its address.
+
+    Parameters:
+    - address (str): The address to search for.
+    - client: An initialized client object to interact with the property service.
+
+    Returns:
+    - dict: A dictionary containing the property's details. If an error occurs, an empty string is returned.
+    """
+    try:
+        # Search for property using its address and extract its URL.
+        response = await client.search(address)
+        url = safe_get(response, 'payload', 'exactMatch', 'url')
+        if not url:
+            raise ValueError(f"No URL found for address: {address}")
+      
+        # Extract initial info, propertyId, and listingId using the URL.
+        initial_info = await client.initial_info(url)
+        property_id = safe_get(initial_info, 'payload', 'propertyId')
+        if not property_id:
+            raise ValueError(f"No property ID found for URL: {url}")
+        
+        listing_id = safe_get(initial_info, 'payload', 'listingId')
+        if not listing_id:
+            raise ValueError(f"No listing ID found for URL: {url}")
+        
+        # Fetch property details using propertyId and listingId.
+        property_request= await client.above_the_fold(property_id,listing_id)
+        price = safe_get(property_request, 'payload', 'addressSectionInfo', 'latestPriceInfo', 'amount', default=-1)
+        status = safe_get(property_request, 'payload', 'addressSectionInfo', 'status', 'displayValue', default='N/A')
+        
+        # Fetch property price history.
+        price_history = await client.avm_historical(property_id, listing_id)
+        statusDate = safe_get(price_history, 'payload', 'avmUpdateDate', default=0) / 1000
+        date_obj = datetime.datetime.fromtimestamp(statusDate)
+        history = safe_get(price_history, 'payload', 'propertyTimeSeries', default=[])
+
+        return{
+            'displayedPrice': price,
+            'history': history,
+            'status': status,
+            'statusDate': date_obj
+        }
+
+    except ValueError as e:  # For custom error messages
+        #print(f"Error encountered: {e}")
+        return ""
+    except Exception as e:   # For unexpected issues
+        #print(f"Unexpected error encountered: {e}")
+        #print(traceback.format_exc())
+        return ""
+    
+def safe_get(data, *keys, default=None):
+    """
+    Safely retrieve a value from a nested dictionary or list.
     
     Parameters:
-    - data: The dictionary to search.
-    - keys: A series of keys to navigate through the dictionary.
-    - default: The value to return if any key is not found. Defaults to None.
-    
+    - data (dict/list): The dictionary or list to navigate.
+    - keys: A series of keys/indices to retrieve the desired value.
+    - default: The value to return if the desired value is not found. Defaults to None.
+
     Returns:
-    - The found value or the default value.
+    - The desired value if found, else the default value.
     """
     for key in keys:
         if isinstance(data, list) and isinstance(key, int):
@@ -78,10 +132,35 @@ def safe_get(data, *keys):
             if key < len(data):
                 data = data[key]
             else:
-                return None
+                return default
         else:
             try:
                 data = data[key]
             except (KeyError, TypeError):
-                return None
+                return default
     return data
+
+async def getDetails(address, client):
+    """
+    Fetch details of a property. Prioritizes closed properties over opened ones for efficiency reasons.
+
+    Parameters:
+    - address (str): The address to search for.
+    - client: An initialized client object to interact with the property service.
+
+    Returns:
+    - dict: A dictionary containing the property's details. If an error occurs, an empty string is returned.
+    """
+    # Fetch closed property details first for efficiency.
+    close_property = await getDetailsClosedProperty(address,client)
+    if close_property != "":
+        return close_property
+    
+    # If closed property data is not available, fetch opened property details.
+    # TODO: This section need further development.
+    #open_property = await getDetailsOpenedProperty(address, client)
+    #if open_property != "":
+    #    return open_property
+
+    return ""
+
