dba0e0aac019e93d1010a0d596a7845ec36ba2f6
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-09-13 20:13:19 -0500

    add dynamic file search feature
    Have user input file in downloads folder to be process

--- geoJsonHelper.py
+++ geoJsonHelper.py
@@ -1,45 +1,60 @@
 import os
 import json
 
+
 def delete_unwanted_files(path):
-    #TODO: some files of states dont have statewide-addresses-state.geojson file
-    # will need to adjust the code to see if the file is in the sub folder of the state
-    # otherwise search through all files in the subfolder and merged them together to create
-    # a statewide-addresses-state.geojson file
+    """
+    Deletes all files in the given directory tree, except for "statewide-addresses-state.geojson".
+    
+    If the desired file doesn't exist in a subfolder, consider merging other files to create one.
+    (Note: The merging functionality is not provided here; a placeholder comment is used instead.)
+    """
+    
+    # Flag to check if the desired file exists in the current directory
+    has_statewide_file = False
+    
     for dirpath, dirnames, filenames in os.walk(path):
         for filename in filenames:
-            # can be modified in the future for just city of the state instead
-            # reason we want statewide-addresses-state file is because the file
-            # contains city values in the json 
-            if filename != "statewide-addresses-state.geojson":
+            if filename == "statewide-addresses-state.geojson":
+                has_statewide_file = True
+            else:
+                # Delete the file if it's not the desired file
                 file_to_delete = os.path.join(dirpath, filename)
                 try:
                     os.remove(file_to_delete)
                     print(f"Deleted: {file_to_delete}")
                 except Exception as e:
                     print(f"Error deleting {file_to_delete}. Error: {e}")
-
-def deleter(folderPath):
+                    
+        # Placeholder: Merge functionality can be added here if `has_statewide_file` is False.
+        if not has_statewide_file:
+            print(f"No 'statewide-addresses-state.geojson' in {dirpath}. Consider merging other files.")
+        has_statewide_file = False  # Reset the flag for the next directory
+        
+def deleter():
+    curdir =os.getcwd()
+    folderPath = os.path.join(curdir,"downloads")
     # folder path is the path to files downloaded from openAddresses
     if os.path.exists(folderPath) and os.path.isdir(folderPath):
         delete_unwanted_files(folderPath)
         print("Finished deleting unwanted files.")
     else:
-        print(f"'{folderPath}' is not a valid directory path.")
-        
-        
-def write_to_file(filename, entry):
-    """Append an entry to a file, with each entry on a new line."""
-    with open(filename, 'a') as f:  # 'a' mode ensures entries are appended and won't overwrite existing content
-        f.write(entry + '\n')  # add a newline after each entry
-        
+        print(f"'{folderPath}' is not a valid directory path.") 
+      
 """ 
     This is a temporary place holder for downloading files from https://batch.openaddresses.io/data.
     As of the moment there is issue with using selenium python library to navigate to the URL and download
     the following files us-northeast, us-south, us-midwest, us-west. There may be another method to download the files
-    but as of this moment it is not possible
+    but as of this moment it is not possible.
+    Might be better to have the user download the files themselves and drop it into downloads
     
 """
+
+def find_file(root_path, target_file):
+    for dirpath, dirnames, filenames in os.walk(root_path):
+        if target_file in filenames:
+            return os.path.join(dirpath, target_file)
+    return None
 
 def is_valid(entry):
     """
@@ -52,17 +67,43 @@ def is_valid(entry):
             return False
     return True
 
-def remove_duplicates(input_file, output_file):
+def find_state_folder(root_path, state):
+    """
+    This function will search through all subdirectories in the root path until
+    it finds a directory that matches the state name.
+    """
+    for dirpath, dirnames, _ in os.walk(root_path):
+        if state in dirnames:
+            return os.path.join(dirpath, state)
+    return None
+
+def remove_duplicates(state):
     # This function cleans up the statewide-addresses-state.geojson file by 
     # 1.) removing duplicate addresses.
     # 2.) removing removing addresses that are from apartments(most likely if the unit field is filed, might also remove false positive addresses)
     # 3.) making sure the addresses from the file have enough fields needed for redfin to find the addresses
     
     # This should help with run time by not wasting time looking for duplicate, apartment, and incomplete addresses.
+    state = state.lower()
+    target_file = "statewide-addresses-state.geojson"
+    curdir = os.getcwd()
+    output_path = os.path.join(curdir, "cleaned")
+    input_path = os.path.join(curdir, "downloads")
+    
+    state_folder = find_state_folder(input_path, state)
+    if not state_folder:
+        print(f"Folder for {state} not found!")
+        return None
+    
+    path = find_file(state_folder, target_file)
+    if path == None:
+        print(f"{target_file} not found in {state} folder!")
+        return None
+    
     unique_entries = set()
     cleaned_data = []
 
-    with open(input_file, 'r') as f:
+    with open(path, 'r') as f:
         for line in f.readlines():
             entry = json.loads(line)
             
@@ -91,11 +132,12 @@ def remove_duplicates(input_file, output_file):
                 cleaned_data.append(line)
 
     # Write the cleaned data to the output file
+    os.chdir(output_path)
+    output_file = state + ".geojson"
     with open(output_file, 'w') as f:
         for line in cleaned_data:
             f.write(line)
-
-
+   
 def read_geojson_features(file_path):
     features = []
     with open(file_path, 'r') as file:
@@ -106,5 +148,3 @@ def read_geojson_features(file_path):
             except json.JSONDecodeError as e:
                 print(f"Failed to decode JSON on line: {line}. Error: {e}")
     return features
-
-
