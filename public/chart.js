6dffc1f45f4b039f7c8abb93220d1f20e0a72c71
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2023-09-19 13:01:53 -0500

    Fix bug for chart.js

--- public/chart.js
+++ public/chart.js
@@ -1,13 +1,51 @@
-function processForChart(dataArray) {
+function getMostFrequentLength(histories) {
+    const frequency = {};
+
+    histories.forEach(history => {
+        const length = history.length;
+        frequency[length] = (frequency[length] || 0) + 1;
+    });
+
+    let maxFrequency = -1;
+    let mostFrequentLength = -1;
+
+    for (const length in frequency) {
+        if (frequency[length] > maxFrequency) {
+            maxFrequency = frequency[length];
+            mostFrequentLength = parseInt(length);
+        }
+    }
+
+    return mostFrequentLength;
+}
+
+function getMonthLabel(date) {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return monthNames[date.getMonth()] + ' ' + date.getFullYear();
+}
+
+function processForChart(dataArray,userInput) {
+    let labelString = `${userInput} Average Property Value`;
+    console.log(dataArray);
     // Convert string histories to arrays
     const allHistories = dataArray.map(entry => JSON.parse(entry.history));
 
-    // Generate labels based on the longest history
-    const maxHistoryLength = Math.max(...allHistories.map(history => history.length));
-    const labels = Array.from({ length: maxHistoryLength }, (_, i) => `Point ${i + 1}`);
+    // Determine the most frequent history length
+    const mostFrequentHistoryLength = getMostFrequentLength(allHistories);
 
-    const averageData = Array.from({ length: maxHistoryLength }).map((_, index) => {
-        const valuesAtCurrentIndex = allHistories.map(history => history[index] || 0);
+    // Filter out histories that don't match the most frequent length
+    const matchingHistories = allHistories.filter(history => history.length === mostFrequentHistoryLength);
+
+    // Generate labels based on the month
+    const endDate = new Date(dataArray[0].dataDate); // Assuming all entries have the same dataDate
+    const labels = Array.from({ length: mostFrequentHistoryLength }).map((_, index) => {
+        const dateToUse = new Date(endDate);
+        dateToUse.setMonth(dateToUse.getMonth() - index); // Subtract months based on the index
+        return getMonthLabel(dateToUse);
+    }).reverse(); // Reverse labels to have them in the correct order
+
+    const averageData = Array.from({ length: mostFrequentHistoryLength }).map((_, index) => {
+        const valuesAtCurrentIndex = matchingHistories.map(history => history[index] || 0);
         const sum = valuesAtCurrentIndex.reduce((acc, value) => acc + value, 0);
         return sum / valuesAtCurrentIndex.length;
     });
@@ -15,13 +53,15 @@ function processForChart(dataArray) {
     return {
         labels: labels,
         datasets: [{
-            label: 'Average Property Value',
+            label: labelString,
             borderColor: '#4e19e0',
             backgroundColor: 'rgba(255, 87, 51, 0.2)',
             data: averageData
         }]
     };
 }
+
+
 function renderChart(data) {
     const config = {
         type: 'line',
